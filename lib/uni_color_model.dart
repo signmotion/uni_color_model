c5c8f4364109ce0241835f8f951010d1119b95d2
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-02 13:14:16 +0200

    Removed int_4 and int_10 color classes. Reason: temporarily excluded to avoid unnecessary duplication during the development phase of converters.

--- lib/uni_color_model.dart
+++ lib/uni_color_model.dart
@@ -17,16 +17,12 @@ part 'src/extensions/string.dart';
 part 'src/extensions/uni_color.dart';
 
 part 'src/models/argb_double.dart';
-part 'src/models/argb_int_4.dart';
 part 'src/models/argb_int_8.dart';
 part 'src/models/cmyk.dart';
 part 'src/models/color.dart';
 part 'src/models/num_color.dart';
 part 'src/models/double.dart';
-part 'src/models/int_4.dart';
 part 'src/models/int_8.dart';
-part 'src/models/int_10.dart';
-part 'src/models/rgb_int_4.dart';
 part 'src/models/rgb_int_8.dart';
 part 'src/models/rgb.dart';
 part 'src/models/rgb_double.dart';
