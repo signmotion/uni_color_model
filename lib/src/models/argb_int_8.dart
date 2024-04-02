d90c06474240629e4fa705640511a7dc3d0718cd
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-02 13:34:19 +0200

    Restricted a null for ArgbInt8Color.argb() factory.

--- lib/src/models/argb_int_8.dart
+++ lib/src/models/argb_int_8.dart
@@ -19,6 +19,6 @@ class ArgbInt8Color extends Int8Color {
           channel4: 0,
         );
 
-  factory ArgbInt8Color.argb(int? a, int r, int g, int b) =>
+  factory ArgbInt8Color.argb(int a, int r, int g, int b) =>
       ArgbInt8Color(channel0: a, channel1: r, channel2: g, channel3: b);
 }
