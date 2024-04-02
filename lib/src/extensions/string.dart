0da8fc611e28c5ce75713da7f4c8aff4c6d11658
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-02 14:58:52 +0200

    Extended ColorModelStringExt. Provided tests.

--- lib/src/extensions/string.dart
+++ lib/src/extensions/string.dart
@@ -1,7 +1,39 @@
 part of '../../uni_color_model.dart';
 
 extension ColorModelStringExt on String {
+  /// The zero (alpha in ARGB model) channel of this color in an 8 bit value.
+  int get int8Channel0Color =>
+      length < 2 ? -1 : int.parse('0x${substring(0, 0 + 2)}');
+
+  /// The first (red in ARGB model) channel of this color in an 8 bit value.
+  int get int8Channel1Color =>
+      length < 4 ? -1 : int.parse('0x${substring(2, 2 + 2)}');
+
+  /// The second (green in ARGB model) channel of this color in an 8 bit value.
+  int get int8Channel2Color =>
+      length < 6 ? -1 : int.parse('0x${substring(4, 4 + 2)}');
+
+  /// The third (blue in ARGB model) channel of this color in an 8 bit value.
+  int get int8Channel3Color =>
+      length < 8 ? -1 : int.parse('0x${substring(6, 6 + 2)}');
+
+  /// The fourth (black in CMYK model) channel of this color in an 8 bit value.
+  int get int8Channel4Color =>
+      length < 10 ? -1 : int.parse('0x${substring(8, 8 + 2)}');
+
+  // /// For example, ARGB.
+  // int get int8Channels0123 => this & 0xffffffff;
+
+  // /// For example, RGB.
+  // int get int8Channels123 => this & 0xffffff;
+
+  // /// For example, CMYK.
+  // int get int8Channels1234 => this & 0xffffffff;
+
+  // /// For example, ACMYK.
+  // int get int8Channels01234 => this & 0xffffffffff;
+
   /// The string with latin characters only.
   String get normalizedNameColor =>
-      replaceAll(RegExp(r'[\W]+'), '').toLowerCase();
+      replaceAll(RegExp(r'[\W]+'), '').trim().toLowerCase();
 }
---
+++ test/extensions/string_test.dart
@@ -0,0 +1,71 @@
+import 'package:uni_color_model/uni_color_model.dart';
+import 'package:test/test.dart';
+
+void main() {
+  // no alpha
+  const b = '0a1b2c';
+  const c = '#0a1b2c';
+  const d = '0a 1b 2c';
+  const e = ' 0a    1b  2c        ';
+
+  // with alpha
+  const ab = 'de0a1b2c';
+  const ac = '#de0a1b2c';
+  const ad = 'de 0a 1b 2c';
+  const ae = ' de  0a    1b  2c      ';
+
+  group('No alpha', () {
+    test('int8Channel0Color', () {
+      expect(b.int8Channel0Color, 0x0a);
+    });
+
+    test('int8Channel1Color', () {
+      expect(b.int8Channel1Color, 0x1b);
+    });
+
+    test('int8Channel2Color', () {
+      expect(b.int8Channel2Color, 0x2c);
+    });
+
+    test('int8Channel3Color', () {
+      expect(b.int8Channel3Color, -1);
+    });
+
+    test('int8Channel4Color', () {
+      expect(b.int8Channel4Color, -1);
+    });
+  }, tags: ['current']);
+
+  group('normalizedNameColor', () {
+    test('latin, lowercase, no spaces', () {
+      expect('green'.normalizedNameColor, 'green');
+      expect('green'.normalizedNameColor, 'green');
+    });
+
+    test('latin, lowercase, spaces', () {
+      expect('forrest green'.normalizedNameColor, 'forrestgreen');
+      expect('  forrest   green '.normalizedNameColor, 'forrestgreen');
+    });
+
+    test('latin, anycase, spaces', () {
+      expect('Forrest Green'.normalizedNameColor, 'forrestgreen');
+    });
+
+    test('latin, anycase, spaces, special symbols', () {
+      expect('Forrest + Green'.normalizedNameColor, 'forrestgreen');
+      expect('Forrest _ Green'.normalizedNameColor, 'forrestgreen');
+      expect('Forrest_Green'.normalizedNameColor, 'forrestgreen');
+      expect('F*rrest | Green'.normalizedNameColor, 'forrestgreen');
+    });
+
+    test('hex digits, anycase, spaces, special symbols', () {
+      expect('#de0A1b2c'.normalizedNameColor, 'de0a1b2c');
+      expect(' #de 0A 1b 2c  '.normalizedNameColor, 'de0a1b2c');
+    });
+
+    test('empty', () {
+      expect(''.normalizedNameColor, '');
+      expect('  '.normalizedNameColor, '');
+    });
+  });
+}
