b03d1fb46c322d28683738902ce6db175b74242e
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-03 20:14:47 +0200

    Added tests for int.rgbWith().

--- test/constructors/int_test.dart
+++ test/constructors/int_test.dart
@@ -10,7 +10,17 @@ void main() {
     test('b.rgb', () {
       final r = b.rgb;
       expect(r.model, ColorModel.rgb);
-      expect(r.int24, b);
+      expect(r.channels, [0x00, 0x00, 0x0a, 0x1b, 0x2c]);
     });
-  }, tags: ['current']);
+
+    test('b.rgbWith, equal channels 4', () {
+      final r = b.rgbWith(channelsDepth: 4);
+      expect(r.channels, [0x00, 0x00, 0x0b, 0x02, 0x0c]);
+    });
+
+    test('b.rgbWith, equal channels 4', () {
+      final r = b.rgbWith(channelsDepth: 4);
+      expect(r.channels, [0x00, 0x00, 0x0b, 0x02, 0x0c]);
+    });
+  });
 }
