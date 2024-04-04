e6349a97ef667892368b568ef1cea7eaa3953550
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-04 13:39:55 +0200

    Added int.intDepthsChannelNColor.

--- lib/src/extensions/int.dart
+++ lib/src/extensions/int.dart
@@ -1,23 +1,34 @@
 part of '../../uni_color_model.dart';
 
-/// TODO(sign): Separated package `int_color`?
+/// TODO(sign): Separated package `int_color`? `bit_extractor`?
 extension ColorModelIntExt on int {
   /// The value of the channel [n] from [int] when the channels have
-  /// sizes [bits].
-  /// [bits] should be in range [1; 64].
+  /// sizes [depth].
+  /// [depth] should be in range [1; 64].
   /// [n] should be in range [0; 64].
-  /// Multiply [n] * [bits] should be less or equal 53 for Web and 64 for others.
+  /// Multiply [n] * [depth] should be less or equal 53 for Web and 64 for others.
   /// [channels] How many channels we have. Range [1; 64]. Default value is 5
   /// because [UniColor] has 5 channels.
   /// [reverse] The first channel starts on the right side of the number.
-  int intBitsChannelNColor(
-    int bits,
+  int intDepthChannelNColor(
+    int depth,
     int n, {
     int channels = 5,
     bool reverse = true,
   }) {
     final k = reverse ? channels - n - 1 : n;
-    return (this >> (bits * k)) & fillWithOnesRight(bits);
+    return (this >> (depth * k)) & fillWithOnesRight(depth);
+  }
+
+  int intDepthsChannelNColor(
+    List<int> depths,
+    int n, {
+    bool reverse = true,
+  }) {
+    final ds = reverse ? depths : depths.reversed.toList(growable: false);
+    final shift = ds.sublist(n + 1).summarize;
+    final l = ds[n];
+    return (this >> shift) & fillWithOnesRight(l);
   }
 
   /// 2 ^ this
--- test/extensions/int_test.dart
+++ test/extensions/int_test.dart
@@ -2,42 +2,93 @@ import 'package:uni_color_model/uni_color_model.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('intBitsChannelNColor, correct values', () {
+  group('intDepthChannelNColor, correct values', () {
     const b = 0x6fde0a1b2c;
-    // bin    0110111111011110000010100001101100101100
-    // bin  2 011011111101111000001010000110 11 00 10 11 00
-    // bin 10 0110111111 0111100000 1010000110 1100101100
+    // bin 0110111111011110000010100001101100101100
 
     test('intBitsChannelNColor 2 bits', () {
-      expect(b.intBitsChannelNColor(2, 0), int.parse('11', radix: 2));
-      expect(b.intBitsChannelNColor(2, 1), int.parse('00', radix: 2));
-      expect(b.intBitsChannelNColor(2, 2), int.parse('10', radix: 2));
-      expect(b.intBitsChannelNColor(2, 3), int.parse('11', radix: 2));
-      expect(b.intBitsChannelNColor(2, 4), int.parse('00', radix: 2));
+      // bin 2 bits 011011111101111000001010000110 11 00 10 11 00
+      expect(b.intDepthChannelNColor(2, 0), int.parse('11', radix: 2));
+      expect(b.intDepthChannelNColor(2, 1), int.parse('00', radix: 2));
+      expect(b.intDepthChannelNColor(2, 2), int.parse('10', radix: 2));
+      expect(b.intDepthChannelNColor(2, 3), int.parse('11', radix: 2));
+      expect(b.intDepthChannelNColor(2, 4), int.parse('00', radix: 2));
     });
 
     test('intBitsChannelNColor 4 bits', () {
-      expect(b.intBitsChannelNColor(4, 0).hex(1), 'a');
-      expect(b.intBitsChannelNColor(4, 1).hex(1), '1');
-      expect(b.intBitsChannelNColor(4, 2).hex(1), 'b');
-      expect(b.intBitsChannelNColor(4, 3).hex(1), '2');
-      expect(b.intBitsChannelNColor(4, 4).hex(1), 'c');
+      expect(b.intDepthChannelNColor(4, 0).hex(1), 'a');
+      expect(b.intDepthChannelNColor(4, 1).hex(1), '1');
+      expect(b.intDepthChannelNColor(4, 2).hex(1), 'b');
+      expect(b.intDepthChannelNColor(4, 3).hex(1), '2');
+      expect(b.intDepthChannelNColor(4, 4).hex(1), 'c');
     });
 
     test('intBitsChannelNColor 8 bits', () {
-      expect(b.intBitsChannelNColor(8, 0).hex(), '6f');
-      expect(b.intBitsChannelNColor(8, 1).hex(), 'de');
-      expect(b.intBitsChannelNColor(8, 2).hex(), '0a');
-      expect(b.intBitsChannelNColor(8, 3).hex(), '1b');
-      expect(b.intBitsChannelNColor(8, 4).hex(), '2c');
+      expect(b.intDepthChannelNColor(8, 0).hex(), '6f');
+      expect(b.intDepthChannelNColor(8, 1).hex(), 'de');
+      expect(b.intDepthChannelNColor(8, 2).hex(), '0a');
+      expect(b.intDepthChannelNColor(8, 3).hex(), '1b');
+      expect(b.intDepthChannelNColor(8, 4).hex(), '2c');
     });
 
     test('intBitsChannelNColor 10 bits', () {
-      expect(b.intBitsChannelNColor(10, 0), int.parse('0000000000', radix: 2));
-      expect(b.intBitsChannelNColor(10, 1), int.parse('0110111111', radix: 2));
-      expect(b.intBitsChannelNColor(10, 2), int.parse('0111100000', radix: 2));
-      expect(b.intBitsChannelNColor(10, 3), int.parse('1010000110', radix: 2));
-      expect(b.intBitsChannelNColor(10, 4), int.parse('1100101100', radix: 2));
+      // bin 10 bits 0110111111 0111100000 1010000110 1100101100
+      expect(b.intDepthChannelNColor(10, 0), int.parse('0000000000', radix: 2));
+      expect(b.intDepthChannelNColor(10, 1), int.parse('0110111111', radix: 2));
+      expect(b.intDepthChannelNColor(10, 2), int.parse('0111100000', radix: 2));
+      expect(b.intDepthChannelNColor(10, 3), int.parse('1010000110', radix: 2));
+      expect(b.intDepthChannelNColor(10, 4), int.parse('1100101100', radix: 2));
+    });
+  });
+
+  group('intDepthChannelNColor, correct values', () {
+    const b = 0x6fde0a1b2c;
+
+    test('intDepthsChannelNColor 5:6 bits, reverse', () {
+      // bin 01101111110111100000101000011 01100 101100
+      const depths = [5, 6];
+      expect(
+        b.intDepthsChannelNColor(depths, 0),
+        int.parse('01100', radix: 2),
+      );
+      expect(
+        b.intDepthsChannelNColor(depths, 1),
+        int.parse('101100', radix: 2),
+      );
+    });
+
+    test('intDepthsChannelNColor 3:4:5:6 bits, reverse', () {
+      // bin 0110111111011110000010 100 0011 01100 101100
+      const depths = [3, 4, 5, 6];
+      expect(
+        b.intDepthsChannelNColor(depths, 0),
+        int.parse('100', radix: 2),
+      );
+      expect(
+        b.intDepthsChannelNColor(depths, 1),
+        int.parse('0011', radix: 2),
+      );
+      expect(
+        b.intDepthsChannelNColor(depths, 2),
+        int.parse('01100', radix: 2),
+      );
+      expect(
+        b.intDepthsChannelNColor(depths, 3),
+        int.parse('101100', radix: 2),
+      );
+    });
+
+    test('intDepthsChannelNColor 5:6 bits, no reverse', () {
+      // bin 01101111110111100000101000011 011001 01100
+      const depths = [5, 6];
+      expect(
+        b.intDepthsChannelNColor(depths, 0, reverse: false),
+        int.parse('10010', radix: 2),
+      );
+      expect(
+        b.intDepthsChannelNColor(depths, 1, reverse: false),
+        int.parse('1100', radix: 2),
+      );
     });
   }, tags: ['current']);
 
