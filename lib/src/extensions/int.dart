6a4ceaf7cf35f2ac606d6e4ffc6c4bedac408631
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-05 14:52:43 +0200

    Optimized int.unpackFromIntDepth() for depth == 8.

--- lib/src/extensions/int.dart
+++ lib/src/extensions/int.dart
@@ -32,10 +32,45 @@ extension ColorModelIntExt on int {
     return (this >> shift) & fillWithOnesRight(l);
   }
 
+  /// See [ColorModelListIntExt.packToInt8].
+  List<int> get unpackFromInt8 => switch (this) {
+        == 0x00 => <int>[],
+        <= 0xff => [
+            0xff & this,
+          ],
+        <= 0xffff => [
+            0xff & (this >> 8),
+            0xff & this,
+          ],
+        <= 0xffffff => [
+            0xff & (this >> 16),
+            0xff & (this >> 8),
+            0xff & this,
+          ],
+        <= 0xffffffff => [
+            0xff & (this >> 24),
+            0xff & (this >> 16),
+            0xff & (this >> 8),
+            0xff & this,
+          ],
+        <= 0xffffffffff => [
+            0xff & (this >> 32),
+            0xff & (this >> 24),
+            0xff & (this >> 16),
+            0xff & (this >> 8),
+            0xff & this,
+          ],
+        _ => throw ArgumentError('Supported int <= 0xffffffffff (5 bytes).'),
+      };
+
   List<int> unpackFromIntDepth(
     int depth, {
     bool reverse = true,
   }) {
+    if (depth == 8 && reverse) {
+      return unpackFromInt8;
+    }
+
     final r = switch (this) {
       == 0x00 => <int>[],
       <= 0xff => [
@@ -109,37 +144,6 @@ extension ColorModelIntExt on int {
 
     return reverse ? r : r.reversed.toList();
   }
-
-  /// See [ColorModelListIntExt.packToInt8].
-  List<int> get unpackFromInt8 => switch (this) {
-        == 0x00 => <int>[],
-        <= 0xff => [
-            0xff & this,
-          ],
-        <= 0xffff => [
-            0xff & (this >> 8),
-            0xff & this,
-          ],
-        <= 0xffffff => [
-            0xff & (this >> 16),
-            0xff & (this >> 8),
-            0xff & this,
-          ],
-        <= 0xffffffff => [
-            0xff & (this >> 24),
-            0xff & (this >> 16),
-            0xff & (this >> 8),
-            0xff & this,
-          ],
-        <= 0xffffffffff => [
-            0xff & (this >> 32),
-            0xff & (this >> 24),
-            0xff & (this >> 16),
-            0xff & (this >> 8),
-            0xff & this,
-          ],
-        _ => throw ArgumentError('Supported int <= 0xffffffffff (5 bytes).'),
-      };
 
   /// 2 ^ this
   int get pow2N => pow(2, this).round();
